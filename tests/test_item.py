0c48ada18e84a22fa7a76bb7d8a98e9663192e75
DimaSouthTree <DITsindrin@gmail.com>
2023-06-04 14:15:14 +0300

    added test_calculate_total_price and added class method calculate_total_price

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,12 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+test_1 = Item("Test_1", 10000, 10)
+test_2 = Item("Test_2", 10000, 0)
+def test_calculate_total_price():
+    assert test_1.calculate_total_price() == 100000
+    assert test_2.calculate_total_price() == 0
+
+def test_apply_discount():
+    ...
