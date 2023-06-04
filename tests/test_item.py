1550247807810c0b001868f0afeb1fcf6eebb33d
DimaSouthTree <DITsindrin@gmail.com>
2023-06-04 14:25:04 +0300

    added test_apply_discount and added class method apply_discount

--- tests/test_item.py
+++ tests/test_item.py
@@ -8,5 +8,9 @@ def test_calculate_total_price():
     assert test_1.calculate_total_price() == 100000
     assert test_2.calculate_total_price() == 0
 
+Item.pay_rate = 0.8
+# применяем скидку
+test_2.apply_discount()
 def test_apply_discount():
-    ...
+    assert test_1.price == 10000
+    assert test_2.price == 8000
